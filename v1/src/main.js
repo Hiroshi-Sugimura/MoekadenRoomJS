18ebdf4166cc3fad23972fe7cb8a9076331666a2
Hiroshi SUGIMURA@kait <hiroshi.sugimura@gmail.com>
2022-09-15 19:12:49 +0900

    Update main.js

--- v1/src/main.js
+++ v1/src/main.js
@@ -69,101 +69,102 @@ function sendDevState() {
 // ECHONET Lite管理
 
 let devState = {
+	'001101': {  // thermometer
+		// super
+		'80': [0x30], // 動作状態, on, get, inf
+		'81': [0x0f], // 設置場所, set, get, inf
+		'82': [0x00, 0x00, 0x50, 0x01],  // spec version, P. rev1, get
+		'83': [0xfe, 0x00, 0x00, 0x77, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x06], // identifier, initialize時に、renewNICList()できちんとセットする, get
+		'88': [0x42], // 異常状態, 0x42 = 異常無, get
+		'8a': [0x00, 0x00, 0x77],  // maker code, kait, get
+		'9d': [0x02, 0x80, 0x81],  // inf map, 1 Byte目は個数, get
+		'9e': [0x02, 0x80, 0x81],  // set map, 1 Byte目は個数, get
+		'9f': [0x0a, 0x80, 0x81, 0x82, 0x83, 0x88, 0x8a, 0x9d, 0x9e, 0x9f, 0xe0], // get map, 1 Byte目は個数, get
+		// uniq
+		'e0': ['00', 'dc']  // 温度計測値, get
+	},
 	'013001': {  // aircon
 		// super
-		'80': [0x31], // 動作状態
-		'81': [0x0f], // 設置場所
-		'82': [0x00, 0x00, 0x50, 0x01],  // spec version, P. rev1
-		'83': [0xfe, 0x00, 0x00, 0x77, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02], // identifier, initialize時に、renewNICList()できちんとセットする
-		'88': [0x42], // 異常状態, 0x42 = 異常無
-		'8a': [0x00, 0x00, 0x77],  // maker code, kait
-		'9d': [0x04, 0x80, 0x8f, 0xa0, 0xb0],  // inf map, 1 Byte目は個数
-		'9e': [0x04, 0x80, 0x8f, 0xa0, 0xb0],  // set map, 1 Byte目は個数
-		'9f': [0x09, 0x80, 0x81, 0x82, 0x88, 0x8a, 0x8f, 0x9d, 0x9e, 0x9f], // get map, 1 Byte目は個数
+		'80': [0x31], // 動作状態, set?, get, inf
+		'81': [0x0f], // 設置場所, set, get, inf
+		'82': [0x00, 0x00, 0x50, 0x01],  // spec version, P. rev1, get
+		'83': [0xfe, 0x00, 0x00, 0x77, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02], // identifier, initialize時に、renewNICList()できちんとセットする, get
+		'88': [0x42], // 異常状態, 0x42 = 異常無, get
+		'8a': [0x00, 0x00, 0x77],  // maker code, kait, ,get
+		'9d': [0x05, 0x80, 0x81, 0x8f, 0xb0, 0xa0],  // inf map, 1 Byte目は個数, get
+		'9e': [0x06, 0x80, 0x81, 0x8f, 0xb0, 0xb3, 0xa0],  // set map, 1 Byte目は個数, get
+		'9f': [0x0d, 0x80, 0x81, 0x82, 0x83, 0x88, 0x8a, 0x8f, 0x9d, 0x9e, 0x9f, 0xb0, 0xb3, 0xbb], // get map, 1 Byte目は個数, get
 		// uniq
-		'8f': [0x42], // 節電動作設定
-		'b0': [0x42], // 運転モード設定
-		'b3': [0x14], // 温度設定
-		'bb': [0x14], // 室内温度計測値
-		'a0': [0x41]  // 風量設定
+		'8f': [0x42], // 節電動作設定, set, get, inf
+		'b0': [0x42], // 運転モード設定, set, get, inf
+		'b3': [0x14], // 温度設定, set, get
+		'bb': [0x14], // 室内温度計測値, get
+		'a0': [0x41]  // 風量設定, set, get, inf
 	},
-	'029001': {  // lighting
+	'026001': {  // blind = curtain, 日よけ
 		// super
-		'80': [0x31], // 動作状態
-		'81': [0x0f], // 設置場所
-		'82': [0x00, 0x00, 0x50, 0x01],  // spec version, P. rev1
-		'83': [0xfe, 0x00, 0x00, 0x77, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03], // identifier, initialize時に、renewNICList()できちんとセットする
-		'88': [0x42], // 異常状態, 0x42 = 異常無
-		'8a': [0x00, 0x00, 0x77],  // maker code, kait
-		'9d': [0x04, 0x80, 0x8f, 0xa0, 0xb0],  // inf map, 1 Byte目は個数
-		'9e': [0x04, 0x80, 0x8f, 0xa0, 0xb0],  // set map, 1 Byte目は個数
-		'9f': [0x09, 0x80, 0x81, 0x82, 0x88, 0x8a, 0x8f, 0x9d, 0x9e, 0x9f], // get map, 1 Byte目は個数
+		'80': [0x30], // 動作状態, on,  get, inf
+		'81': [0x0f], // 設置場所, set, get, inf
+		'82': [0x00, 0x00, 0x50, 0x01],  // spec version, P. rev1, get
+		'83': [0xfe, 0x00, 0x00, 0x77, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04], // identifier, initialize時に、renewNICList()できちんとセットする, get
+		'88': [0x42], // 異常状態, 0x42 = 異常無, get
+		'8a': [0x00, 0x00, 0x77],  // maker code, kait, get
+		'9d': [0x03, 0x80, 0x81, 0xe0],  // inf map, 1 Byte目は個数, get
+		'9e': [0x02, 0x81, 0xe0],  // set map, 1 Byte目は個数, get
+		'9f': [0x0a, 0x80, 0x81, 0x82, 0x83, 0x88, 0x8a, 0x9d, 0x9e, 0x9f, 0xe0], // get map, 1 Byte目は個数, get
 		// uniq
-		'b6': [0x42] // 点灯モード設定
-	},
-	'026001': {  // blind = curtain
-		// super
-		'80': [0x30], // 動作状態, on
-		'81': [0x0f], // 設置場所
-		'82': [0x00, 0x00, 0x50, 0x01],  // spec version, P. rev1
-		'83': [0xfe, 0x00, 0x00, 0x77, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04], // identifier, initialize時に、renewNICList()できちんとセットする
-		'88': [0x42], // 異常状態, 0x42 = 異常無
-		'8a': [0x00, 0x00, 0x77],  // maker code, kait
-		'9d': [0x04, 0x80, 0x8f, 0xa0, 0xb0],  // inf map, 1 Byte目は個数
-		'9e': [0x04, 0x80, 0x8f, 0xa0, 0xb0],  // set map, 1 Byte目は個数
-		'9f': [0x09, 0x80, 0x81, 0x82, 0x88, 0x8a, 0x8f, 0x9d, 0x9e, 0x9f], // get map, 1 Byte目は個数
-		// uniq
-		'e0': [0x41]
+		'e0': [0x41]  // 開閉動作設定, set, get, inf
 	},
 	'026f01': {  // electnic lock
 		// super
-		'80': [0x30], // 動作状態, on
-		'81': [0x0f], // 設置場所
-		'82': [0x00, 0x00, 0x50, 0x01],  // spec version, P. rev1
-		'83': [0xfe, 0x00, 0x00, 0x77, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05], // identifier, initialize時に、renewNICList()できちんとセットする
-		'88': [0x42], // 異常状態, 0x42 = 異常無
-		'8a': [0x00, 0x00, 0x77],  // maker code, kait
-		'9d': [0x04, 0x80, 0x8f, 0xa0, 0xb0],  // inf map, 1 Byte目は個数
-		'9e': [0x04, 0x80, 0x8f, 0xa0, 0xb0],  // set map, 1 Byte目は個数
-		'9f': [0x09, 0x80, 0x81, 0x82, 0x88, 0x8a, 0x8f, 0x9d, 0x9e, 0x9f], // get map, 1 Byte目は個数
+		'80': [0x30], // 動作状態, on, get, inf
+		'81': [0x0f], // 設置場所, set, get, inf
+		'82': [0x00, 0x00, 0x50, 0x01],  // spec version, P. rev1, get
+		'83': [0xfe, 0x00, 0x00, 0x77, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05], // identifier, initialize時に、renewNICList()できちんとセットする, get
+		'88': [0x42], // 異常状態, 0x42 = 異常無, get
+		'8a': [0x00, 0x00, 0x77],  // maker code, kait, get
+		'9d': [0x03, 0x80, 0x81, 0xe0],  // inf map, 1 Byte目は個数, get
+		'9e': [0x02, 0x81, 0xe0],  // set map, 1 Byte目は個数, get
+		'9f': [0x0a, 0x80, 0x81, 0x82, 0x83, 0x88, 0x8a, 0x9d, 0x9e, 0x9f, 0xe0], // get map, 1 Byte目は個数, get
 		// uniq
-		'e0': [0x41]  // 施錠設定１
-	},
-	'001101': {  // thermometer
-		// super
-		'80': [0x30], // 動作状態, on
-		'81': [0x0f], // 設置場所
-		'82': [0x00, 0x00, 0x50, 0x01],  // spec version, P. rev1
-		'83': [0xfe, 0x00, 0x00, 0x77, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x06], // identifier, initialize時に、renewNICList()できちんとセットする
-		'88': [0x42], // 異常状態, 0x42 = 異常無
-		'8a': [0x00, 0x00, 0x77],  // maker code, kait
-		'9d': [0x04, 0x80, 0x8f, 0xa0, 0xb0],  // inf map, 1 Byte目は個数
-		'9e': [0x04, 0x80, 0x8f, 0xa0, 0xb0],  // set map, 1 Byte目は個数
-		'9f': [0x09, 0x80, 0x81, 0x82, 0x88, 0x8a, 0x8f, 0x9d, 0x9e, 0x9f], // get map, 1 Byte目は個数
-		// uniq
-		'e0': ['00', 'dc']  // 温度計測値
+		'e0': [0x41]  // 施錠設定１, set, get, inf
 	},
 	'028801': {  // smart meter
 		// super
-		'80': [0x30], // 動作状態, on
-		'81': [0x0f], // 設置場所
-		'82': [0x00, 0x00, 0x50, 0x01],  // spec version, P. rev1
-		'83': [0xfe, 0x00, 0x00, 0x77, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x07], // identifier, initialize時に、renewNICList()できちんとセットする
-		'88': [0x42], // 異常状態, 0x42 = 異常無
-		'8a': [0x00, 0x00, 0x77],  // maker code, kait
-		'9d': [0x04, 0x80, 0x8f, 0xa0, 0xb0],  // inf map, 1 Byte目は個数
-		'9e': [0x04, 0x80, 0x8f, 0xa0, 0xb0],  // set map, 1 Byte目は個数
-		'9f': [0x09, 0x80, 0x81, 0x82, 0x88, 0x8a, 0x8f, 0x9d, 0x9e, 0x9f], // get map, 1 Byte目は個数
+		'80': [0x30], // 動作状態, on, get, inf
+		'81': [0x0f], // 設置場所, set, get, inf
+		'82': [0x00, 0x00, 0x50, 0x01],  // spec version, P. rev1, get
+		'83': [0xfe, 0x00, 0x00, 0x77, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x07], // identifier, initialize時に、renewNICList()できちんとセットする, get
+		'88': [0x42], // 異常状態, 0x42 = 異常無, get
+		'8a': [0x00, 0x00, 0x77],  // maker code, kait, get
+		'9d': [0x02, 0x80, 0x81],  // inf map, 1 Byte目は個数, get
+		'9e': [0x02, 0x81, 0xa5],  // set map, 1 Byte目は個数, get
+		// '9f': [0x12, 0x80, 0x81, 0x82, 0x83, 0x88, 0x8a, 0x9d, 0x9e, 0x9f, 0xd3, 0xd7, 0xe0, 0xe1, 0xe2, 0xe5, 0xe7, 0xe8, 0xea], // get map, 1 Byte目は個数, 記述形式1はprop16まで, get
+		'9f': [0x12, 65, 65, 65, 33, 0, 64, 0, 96, 65, 0, 65, 0, 0, 0, 2, 2, 2], // get map, 1 Byte目は個数, 記述形式2, get
 		// uniq
-		'd3': [0x00, 0x00, 0x00, 0x01],  // 係数
-		'd7': [0x08],  // 積算電力量有効桁数
-		'e0': [0x02],  // 積算電力量計測値（正）
-		'e1': [0x02],  // 積算電力量単位（正）, 0x02 = 0x01kWh
-		'e2': [], // 積算電力量計測値履歴１（正）
-		'e5': [0x00], // 積算履歴収集日１
-		'e7': [0x10], // 瞬時電力計測値
-		'e8': [0x00, 0x10, 0x00, 0x00], // 瞬時電力計測値
-		'ea': []  // 定時積算電力量計測値
+		'd3': [0x00, 0x00, 0x00, 0x01],  // 係数, Get
+		'd7': [0x08],  // 積算電力量有効桁数, get
+		'e0': [0x02],  // 積算電力量計測値（正）, get
+		'e1': [0x02],  // 積算電力量単位（正）, 0x02 = 0x01kWh, get
+		'e2': [], // 積算電力量計測値履歴１（正）, get
+		'e5': [0x00], // 積算履歴収集日１, set, get
+		'e7': [0x10], // 瞬時電力計測値, get
+		'e8': [0x00, 0x10, 0x00, 0x00], // 瞬時電力計測値, get
+		'ea': []  // 定時積算電力量計測値, get
+	},
+	'029001': {  // lighting
+		// super
+		'80': [0x31], // 動作状態, set?, get, inf
+		'81': [0x0f], // 設置場所, set, get, inf
+		'82': [0x00, 0x00, 0x50, 0x01],  // spec version, P. rev1, get
+		'83': [0xfe, 0x00, 0x00, 0x77, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03], // identifier, initialize時に、renewNICList()できちんとセットする, get
+		'88': [0x42], // 異常状態, 0x42 = 異常無, get
+		'8a': [0x00, 0x00, 0x77],  // maker code, kait, get
+		'9d': [0x04, 0x80, 0x81],  // inf map, 1 Byte目は個数, get
+		'9e': [0x04, 0x80, 0x81, 0xb6],  // set map, 1 Byte目は個数, get
+		'9f': [0x0a, 0x80, 0x81, 0x82, 0x83, 0x88, 0x8a, 0x9d, 0x9e, 0x9f, 0xb6], // get map, 1 Byte目は個数, get
+		// uniq
+		'b6': [0x42] // 点灯モード設定, set, get
 	}
 };
 
@@ -178,19 +179,119 @@ let getAircon = function(rinfo, els) {
 	}
 };
 
-let setAircon = function(rinfo, els) {
-	for (let epc in els.DETAILs) {
-		if (devState['013001'][epc]) { // 持ってるEPCのとき
-			devState['013001'][epc] = els.DETAILs[epc];
-			EL.replyOPC1(rinfo.address, EL.toHexArray(els.TID), EL.toHexArray(els.DEOJ), EL.toHexArray(els.SEOJ), EL.SET_RES, EL.toHexArray(epc), devState['013001'][epc]);
-		} else { // 持っていないEPCのとき, SNA
-			if( els.ESV == EL.SETC ) {
-				EL.replyOPC1(rinfo.address, EL.toHexArray(els.TID), EL.toHexArray(els.DEOJ), EL.toHexArray(els.SEOJ), EL.SETC_SNA, EL.toHexArray(epc), [0x00]);
-			}else{
-				EL.replyOPC1(rinfo.address, EL.toHexArray(els.TID), EL.toHexArray(els.DEOJ), EL.toHexArray(els.SEOJ), EL.SETI_SNA, EL.toHexArray(epc), [0x00]);
-			}
+let setAirconSub = function(rinfo, els, epc, edt) {
+	switch (epc) { // 持ってるEPCのとき
+		// super
+		case '80':  // 動作状態, set?, get, inf
+		// console.log( 'setAirconSub case:', epc, edt);
+		if( edt == '30' || edt == '31' ) {
+			devState['013001'][epc] = [parseInt(edt, 16)];
+			// console.log( 'setAirconSub edt:', edt, devState['013001'][epc]);
+			EL.sendOPC1( EL.EL_Multi, EL.toHexArray(els.DEOJ), [0x05, 0xff, 0x01], EL.INF, EL.toHexArray(epc), [parseInt(edt, 16)] );  // INF
+			return true;
+		}else{
+			return false;
 		}
+		break;
+
+		case '81':  // 設置場所, set, get, inf
+		devState['013001'][epc] = [parseInt(edt, 16)];
+		EL.sendOPC1( EL.EL_Multi, EL.toHexArray(els.DEOJ), [0x05, 0xff, 0x01], EL.INF, EL.toHexArray(epc), [parseInt(edt, 16)] );  // INF
+		return true;
+		break;
+
+		// detail
+		case '8f': // 節電動作設定, set, get, inf
+		if( edt == '41' || edt == '42' ) {
+			devState['013001'][epc] = [parseInt(edt, 16)];
+			EL.sendOPC1( EL.EL_Multi, EL.toHexArray(els.DEOJ), [0x05, 0xff, 0x01], EL.INF, EL.toHexArray(epc), [parseInt(edt, 16)] );  // INF
+			return true;
+		}else{
+			return false;
+		}
+		break;
+
+		case 'b0': // 運転モード設定, set, get, inf
+		switch( edt ) {
+			case '40': // その他
+			case '41': // 自動
+			case '42': // 冷房
+			case '43': // 暖房
+			case '44': // 除湿
+			case '45': // 送風
+			devState['013001'][epc] = [parseInt(edt, 16)];
+			EL.sendOPC1( EL.EL_Multi, EL.toHexArray(els.DEOJ), [0x05, 0xff, 0x01], EL.INF, EL.toHexArray(epc), [parseInt(edt, 16)] );  // INF
+			return true;
+			break;
+
+			default:
+			return false;
+		}
+		break;
+
+		case 'b3': // 温度設定, set, get
+		let temp = parseInt( edt, 16 );
+		if( -1 < temp && temp < 51 ) {
+			devState['013001'][epc] = [temp];
+			return true;
+		}else{
+			return false;
+		}
+		break;
+
+		case 'a0': // 風量設定, set, get, inf
+		switch( edt ) {
+			case '31': // 0x31..0x38の8段階
+			case '32': // 0x31..0x38の8段階
+			case '33': // 0x31..0x38の8段階
+			case '34': // 0x31..0x38の8段階
+			case '35': // 0x31..0x38の8段階
+			case '36': // 0x31..0x38の8段階
+			case '37': // 0x31..0x38の8段階
+			case '38': // 0x31..0x38の8段階
+			case '41': // 自動
+			devState['013001'][epc] = [parseInt(edt, 16)];
+			EL.sendOPC1( EL.EL_Multi, EL.toHexArray(els.DEOJ), [0x05, 0xff, 0x01], EL.INF, EL.toHexArray(epc), [parseInt(edt, 16)] );  // INF
+			return true;
+			break;
+			default:
+			// EDTがおかしい
+			return false;
+		}
+		break;
+
+		default: // 持っていないEPCやset不可能のとき, SNA
+		return false;
 	}
+};
+
+let setAircon = async function(rinfo, els) {
+	let success = true;
+	let retDetails = [];
+	let ret_opc = 0;
+	console.log( 'Recv DETAILs:', els.DETAILs );
+	for (let epc in els.DETAILs) {
+		console.log( 'Now:', epc, devState['013001'][epc] );
+
+		if( await setAirconSub( rinfo, els, epc, els.DETAILs[epc] ) ) {
+			console.log( 'New:', epc, devState['013001'][epc] );
+			retDetails.push( parseInt(epc,16) );  // epcは文字列なので
+			retDetails.push( devState['013001'][epc].length );
+			retDetails.push( devState['013001'][epc] );
+			console.log( 'retDetails:', retDetails );
+		}else{
+			console.log( 'failed:', epc, devState['013001'][epc] );
+			retDetails[epc] = [0x00];
+			success = false;
+		}
+		ret_opc += 1;
+	}
+
+	let ret_esv = success? 0x71: 0x51;  // 一つでも失敗したらSNA
+
+	let arr = [0x10, 0x81, EL.toHexArray(els.TID), EL.toHexArray(els.DEOJ), EL.toHexArray(els.SEOJ), ret_esv, ret_opc, retDetails ];
+	console.dir( arr.flat() ) ;
+	EL.sendArray( rinfo.address, arr.flat() );
 };
 
 // ライト
@@ -329,9 +430,7 @@ let ELStart = async function() {
 						(rinfo, els, err) => {  // els received, 受信のたびに呼ばれる
 							config.debug?console.log( new Date().toFormat("YYYY-MM-DDTHH24:MI:SS"), '| main.ELStart():', els):0;
 
-							if( els ) {
-								console.dir( els );
-							}
+							// if( els ) { console.dir( els ); }
 
 							switch( els.ESV ) {
 								case '60':  // SET_I
